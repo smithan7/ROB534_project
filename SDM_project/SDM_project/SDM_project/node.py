9b3e7439b3fed10a4d034a13f210bde2aea67b2f
smithan7 <smithan7@oregonstate.edu>
2017-03-10 13:20:31 -0800

    updated a lot

--- SDM_project/SDM_project/SDM_project/node.py
+++ SDM_project/SDM_project/SDM_project/node.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+from copy import deepcopy
 from operator import attrgetter
 from task import Task
 
@@ -10,7 +11,7 @@ class Node(object):
         self.x = arg[0]
         self.y = arg[1]
         self.searchDepth = arg[2]
-        self.tasks = arg[3]
+        self.patients = arg[3]
         # value is the cumulative value along the path, we break this into three parts
         ### valueAbove: value from the root to my parent, not really used as we search down
         self.valueAbove = arg[4]
@@ -25,14 +26,18 @@ class Node(object):
         ## for sampling the tree
         self.nSamples = -1
 
-        self.taskIndex = arg[6]
+        self.current_time = arg[6]
+        self.simulation_time = arg[7]
+
+        self.patientIndex = arg[8]
+        self.taskIndex = arg[9]
 
         ## for UCT
         self.nPulls = 0 # how many times have i been searched
 
         self.children = [] # my kids!
-        self.maxSearchDepth = 3 # how deep do I search the tree
-        self.maxRollOutIters = 1 # how deep is my rollout horizon
+        self.maxSearchDepth = 5 # how deep do I search the tree
+        self.maxRollOutIters = 2 # how deep is my rollout horizon
         
     def generateQ( self, args):
         # this will be used to get the probability of an action being completed at a certain time.
@@ -46,6 +51,8 @@ class Node(object):
         # This should cause me to stay away from areas I am unlikely to get to before another agent and should cause the paths to converge.
         path = args[0]
 
+    def updatePatients( self, arg ):
+        self.patients = deepcopy( arg )
 
     def sampleTreeEpsilonGreedy( self, args ):
         # this needs to eplore the tree with some level of greedyness ( to select better paths more frequently or even the optimal path or completely random path as desired ) and 
@@ -128,82 +135,85 @@ class Node(object):
             goldenChild = max(self.children, key=attrgetter('value') )
             path = goldenChild.exploitTree( path )
         
-        path.append( [self.x, self.y] )
+        path.append( [self.patientIndex, self.taskIndex] )
         return path
 
-    def findChildren(self):
+    def findChildren(self, arg):
+        current_time = arg[0]
+        simulation_time = arg[1]
         # takes in tasklist and creates a new child for each task - tasks represent possible actions 
-        #print("nTasks: ", len(self.tasks) )
-        for task in self.tasks:
-            #print("in task loop")
-            tempTasks = list(self.tasks)
-            tempTasks.remove(task)
-            d = math.sqrt( pow(task.x - self.x,2) + pow(task.y - self.y,2) )
-            newChild = Node([task.x, task.y, self.searchDepth+1, tempTasks, self.valueAbove + self.value, task.reward - d, task.index])
-            # rollout child to get value of child
-            newChild.valueBelow = newChild.greedyRollout()
-            newChild.updateValue()
-            # add to children
-            self.children.append( newChild )
+        for p, patient in enumerate(self.patients):
+            d = math.sqrt( pow(patient.x - self.x,2) + pow(patient.y - self.y,2) )
+            sim_time = simulation_time + d
+            patient.updateRewards( sim_time - current_time )
+            for t in range(patient.n_tasks):
+                # only accept children with some reward
+                if patient.rewards[t] > 0:
+                    tempPatients = deepcopy(self.patients)
+                    tempPatients[p].completeTask( t )
+                    newChild = Node([patient.x, patient.y, self.searchDepth+1, tempPatients, self.valueAbove + self.value, patient.rewards[t] - d, current_time, sim_time, p, t])
+                    # rollout child to get value of child
+                    newChild.valueBelow = newChild.greedyRollout( [current_time, sim_time] )
+                    newChild.updateValue()
+                    # add to children
+                    self.children.append( newChild )
+        self.children.append(Node([self.x, self.y, self.searchDepth+1, self.patients, self.valueAbove + self.value, 0.01, current_time, simulation_time + 1, self.patientIndex, self.taskIndex]) ) # always have the option not to move
+        
 
-    def greedyRollout(self):
+    def greedyRollout(self, arg):
         # simple policy used to evalaute an action by greedily selecting remaining actions through a horizon, selecting actions with highest value (reward - cost)
-        tempTasks = list(self.tasks)
+        current_time = arg[0]
+        sim_time = arg[1]
+        tempPatients = deepcopy(self.patients)
         rolloutValue = 0
         curX = self.x
         curY = self.y
         rollOutIters = 0
-        while len(tempTasks) > 0 and rollOutIters < self.maxRollOutIters:
-            rollOutIters = rollOutIters + 1
-            maxValue = -float('inf')
-            maxTask = tempTasks[0]
-            for task in tempTasks:
-                tV = task.reward - math.sqrt( pow(curX - task.x,2) + pow(curY - task.y,2) )
-                if tV > maxValue:
-                    maxValue = tV
-                    maxTask = task
-            rolloutValue = rolloutValue + maxValue
-            curX = maxTask.x
-            curY = maxTask.y
-            tempTasks.remove( maxTask )
+        cumReward = self.getAvailableReward( tempPatients )
 
-        #print("rollout value: ", rolloutValue)
+        while cumReward > 0 and rollOutIters < self.maxRollOutIters:
+            cumReward = self.getAvailableReward( tempPatients )
+            rollOutIters = rollOutIters + 1
+            maxValue = 0.01
+            maxPatient = -1
+            maxTask = -1
+            
+            for p, patient in enumerate(tempPatients):
+                dist = math.sqrt( pow(curX - tempPatients[p].x,2) + pow(curY - tempPatients[p].y,2) )
+                dt = sim_time + dist - current_time
+                patient.updateRewards( dt )
+                for t in range(0, patient.n_tasks):
+                    tV = patient.rewards[t]
+                    if tV == None:
+                        patient.updateRewards( dt )
+                        tV = patient.rewards[t]
+                    if tV > maxValue:
+                        maxValue = tV
+                        maxPatient = p
+                        maxTask = t
+            if maxPatient != -1 and maxTask != -1:
+                rolloutValue = rolloutValue + maxValue
+                curX = tempPatients[maxPatient].x
+                curY = tempPatients[maxPatient].y
+                tempPatients[maxPatient].completeTask( maxTask )
+            else:
+                rolloutValue = rolloutValue + maxValue
+                curX = self.x
+                curY = self.y
+
         return rolloutValue
 
-
-    def epsilonGreedySearch(self, arg):
-        tempValueBelow = -float("inf")
-        if len(self.children) > 0:
-            # if I have children to search
-            # with p(epsilon) choose the best child for expansion, else randomly select a child to expand
-            epsilon = arg
-            if random.random() < epsilon:
-                # get best child and continue search
-                goldencChild = max(self.children, key=attrgetter('value') )
-                #print("nChildren: ", len(self.children) )
-                tempValueBelow = goldencChild.epsilonGreedySearch(epsilon)
-            else:
-                # get random child and continue search
-                gc = random.randint(0, len(self.children)-1 );
-                #print("nChildren: ", len(self.children), "; gc = :", gc )
-                tempValueBelow = self.children[gc].epsilonGreedySearch(epsilon)
-        else:
-            if self.searchDepth < self.maxSearchDepth:
-                # don't have children, make some if I'm not past max depth
-                self.findChildren()
-                if len(self.children) > 0:
-                    #print("nChildren: ", len(self.children) )
-                    tempValueBelow = max(child.value for child in self.children)
-                else:
-                    tempValueBelow = 0
-        # update my valueBelow if a child has better value than is currently known
-        if tempValueBelow > self.valueBelow:
-            self.valueBelow = tempValueBelow
-            self.updateValue()
-
-        return self.value
+    def getAvailableReward(self, arg):
+        tempPatients = arg
+        cumReward = 0
+        for patient in tempPatients:
+            for t in range(0,patient.n_tasks):
+                cumReward += patient.rewards[t]
+        return cumReward
     
-    def uctSearch(self):
+    def uctSearch(self, arg):
+        current_time = arg[0]
+        sim_time = arg[1]
         # use UCT (upper confidence bound for trees) to select child to search
         tempValueBelow = -float("inf")
         if self.searchDepth < self.maxSearchDepth:
@@ -225,11 +235,11 @@ class Node(object):
                     else: # child has NOT been pulled, pull child
                         goldenChild = child
                         break
-
-                tempValueBelow = goldenChild.uctSearch()
+                dist = math.sqrt( pow(self.x - goldenChild.x,2) + pow(self.y - goldenChild.y,2) )
+                tempValueBelow = goldenChild.uctSearch( [current_time, sim_time + dist]  )
             else:
                 # don't have children, make some!
-                self.findChildren()
+                self.findChildren( [current_time, sim_time] )
                 if len(self.children) > 0:
                     tempValueBelow = max(child.value for child in self.children)
                 else:
@@ -263,16 +273,37 @@ class Node(object):
 
         return self.value
 
+    def epsilonGreedySearch(self, arg):
+        tempValueBelow = -float("inf")
+        if len(self.children) > 0:
+            # if I have children to search
+            # with p(epsilon) choose the best child for expansion, else randomly select a child to expand
+            epsilon = arg
+            if random.random() < epsilon:
+                # get best child and continue search
+                goldencChild = max(self.children, key=attrgetter('value') )
+                #print("nChildren: ", len(self.children) )
+                tempValueBelow = goldencChild.epsilonGreedySearch(epsilon)
+            else:
+                # get random child and continue search
+                gc = random.randint(0, len(self.children)-1 );
+                #print("nChildren: ", len(self.children), "; gc = :", gc )
+                tempValueBelow = self.children[gc].epsilonGreedySearch(epsilon)
+        else:
+            if self.searchDepth < self.maxSearchDepth:
+                # don't have children, make some if I'm not past max depth
+                self.findChildren()
+                if len(self.children) > 0:
+                    #print("nChildren: ", len(self.children) )
+                    tempValueBelow = max(child.value for child in self.children)
+                else:
+                    tempValueBelow = 0
+        # update my valueBelow if a child has better value than is currently known
+        if tempValueBelow > self.valueBelow:
+            self.valueBelow = tempValueBelow
+            self.updateValue()
 
-    def updateTasks(self, args):
-        # update tasks robot knows about
-        self.tasks = args
-
-    def appendTasks(self, args):
-        # add a new task
-        self.tasks.append( args )
-
-
+        return self.value
 
 
 
